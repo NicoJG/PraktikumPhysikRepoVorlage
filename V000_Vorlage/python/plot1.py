f753e59c719e3f7f6c868d705e344d5675f11a6b
Nico Guth <nico.guth@tu-dortmund.de>
2021-04-12 00:52:53 +0200

    kürzere Python Vorlage

--- V000_Vorlage/python/plot1.py
+++ V000_Vorlage/python/plot1.py
@@ -1,13 +1,10 @@
 # wechsle die Working Directory zum Versuchsordner, damit das Python-Script von überall ausgeführt werden kann
 import os
-import pathlib
-os.chdir(pathlib.Path(__file__).absolute().parent.parent.__str__())
+os.chdir(os.path.dirname(__file__)+'/../')
 # benutze die matplotlibrc und header-matplotlib.tex Dateien aus dem default Ordner
-# Folgendes auskommentieren, wenn es schneller gehen soll (dafür weniger schön)
-os.environ['MATPLOTLIBRC'] = (pathlib.Path(__file__).absolute().parent.parent.parent / 'default' / 'python' / 'matplotlibrc').__str__()
-os.environ['TEXINPUTS'] =  (pathlib.Path(__file__).absolute().parent.parent.parent / 'default' / 'python').__str__() + ':'
+os.environ['MATPLOTLIBRC'] = os.path.dirname(__file__)+'/../../default/python/matplotlibrc'
+os.environ['TEXINPUTS'] =  os.path.dirname(__file__)+'/../../default/python/:'
 
 # Imports
 import numpy as np
 import matplotlib.pyplot as plt
-
